d6560badab22575e5138139d40e93ff14fb1e2be
Anker <george.anker@mclaren.com>
2021-11-08 22:04:14 +0000

    - Added chrome options back in

--- tests_e2e/test_e2e.py
+++ tests_e2e/test_e2e.py
@@ -35,9 +35,9 @@ def app_with_temp_board():
 @pytest.fixture(scope="module")
 def driver():
 	chrome_options = Options()
-	#chrome_options.add_argument('--headless')
-	#chrome_options.add_argument('--no-sandbox')
-	#chrome_options.add_argument('--disable-dev-shm-usage')
+	chrome_options.add_argument('--headless')
+	chrome_options.add_argument('--no-sandbox')
+	chrome_options.add_argument('--disable-dev-shm-usage')
 	with webdriver.Chrome(executable_path=ChromeDriverManager().install()) as driver:
 		yield driver
 
